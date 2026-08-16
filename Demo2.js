5ae5e91644df65ee92bf5baca85e40f8386e9909
unknown <itspratikshah25.1990@gmail.com>
2026-08-16 12:06:59 +0530

    Test case writen by Pratiksha automation tester

---
+++ Demo2.js
@@ -0,0 +1,8 @@
+console.log("Pratiksha automation tester has written new test cases 1");
+console.log("Pratiksha automation tester has written new test cases 1");
+console.log("Pratiksha automation tester has written new test cases 1");
+console.log("Pratiksha automation tester has written new test cases 1");
+console.log("Pratiksha automation tester has written new test cases 1");
+console.log("Pratiksha automation tester has written new test cases 1");
+console.log("Pratiksha automation tester has written new test cases 1");
+console.log("Pratiksha automation tester has written new test cases 1");
